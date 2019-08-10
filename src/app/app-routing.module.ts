4f514fbd573a66c8f1a54830af3fbf3d863ae471
Arnaldo Almeida <arnaldobichara@gmail.com>
2019-08-09 21:24:26 -0300

    primeira alteração

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ProblemasComponent } from './problemas/problemas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'problemas', component: ProblemasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
