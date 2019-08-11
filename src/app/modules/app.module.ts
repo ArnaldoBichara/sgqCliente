0116de7289a905d04e8ff9e02edc4522daea9c60
Arnaldo Almeida <arnaldobichara@gmail.com>
2019-08-11 19:07:48 -0300

    integrando login

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +14,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProblemasComponent } from './problemas/problemas.component';
 import { IdentityComponent } from './shared/components/identity/identity.component';
+
+// Services
+import { DataService } from './shared/services/data.service';
+import { SecurityService } from './shared/services/security.service';
+import { ConfigurationService } from './shared/services/configuration.service';
+import { StorageService } from './shared/services/storage.service';
+
+
 
 
 @NgModule({
@@ -20,9 +34,23 @@ import { IdentityComponent } from './shared/components/identity/identity.compone
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    NgbModule,
+    // tslint:disable-next-line: deprecation
+    HttpModule,
+    // tslint:disable-next-line: deprecation
+    JsonpModule
   ],
-  providers: [],
+  providers: [
+    DataService,
+    SecurityService,
+    ConfigurationService,
+    StorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
