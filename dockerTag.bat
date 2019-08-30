d17e7fee558a464ce7c5a935c553745fbc8727f4
Arnaldo Almeida <arnaldobichara@gmail.com>
2019-08-30 19:24:41 -0300

    melhorias docker-compose

---
+++ dockerTag.bat
@@ -0,0 +1,6 @@
+docker tag sgq/problemas.api:dev arnaldobichara/sgqproblemas:dev
+docker tag sgq/identity.api:dev arnaldobichara/sgqidentity:dev
+docker tag sgq/workflow.api:dev arnaldobichara/sgqworkflow:dev
+docker tag sgq/ocelotapigw:dev arnaldobichara/ocelotapigw:dev
+docker tag sgq/identity.api:dev arnaldobichara/sgqidentity:dev
+docker tag sgq/sgqclient:v1.0 arnaldobichara/sgqclient:
